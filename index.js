59747b56e9b8b58ea647069ff179342a9254d9b7
pranaysolse <solsepranay0@gmail.com>
2020-11-21 18:52:29 +0530

    first commit for hosting

---
+++ index.js
@@ -0,0 +1,16 @@
+/* 
+
+    image at left side
+    nav bar
+    sections
+    article - education details, project details, contacts and social media details, skills details
+    footer
+   
+   
+ */
+
+
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+});
+      
